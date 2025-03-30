89aec3eb57b3a416fa038780aabaac7f0d5ba77e
Abhinandan Parhi <127218863+abhinandansgit@users.noreply.github.com>
2025-03-30 16:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def contact():
 
 from routes import *
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Ensure it runs on the correct port
+    app.run(host="0.0.0.0", port=port)
